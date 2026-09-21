e321d0fb77763400d733bf68521519bfe7385cad
Kiruseczek <petr.michalik@tria-tr.cz>
2026-09-21 09:29:09 +0200

    4komiit

--- lekce1.py
+++ lekce1.py
@@ -1,1 +1,8 @@
-print("ahoj")
+celacislo = 20
+realnecislo  = 20.5
+cislopi = 3.14
+velkecislo = 1_000_000_000
+hexicimanlnicislo = 0xff
+
+print("celecislo:",celacislo)
+print("hexicimanlnicislo:" ,hexicimanlnicislo )
